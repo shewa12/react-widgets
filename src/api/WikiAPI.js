f297098683b6d824a1a8a6165ee4930246cf49fe
shewa12 <shewa12kpi@gmail.com>
2022-10-23 20:25:51 +0600

    Revert "new: navigation added"
    This reverts commit 4b3a91c003a867570cc2a0b2880765007e71e15b.

--- src/api/WikiAPI.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-const wikiInstance = axios.create({
-    baseURL: 'https://en.wikipedia.org/w/api.php?origin=*',
-    // timeout: 1000,
-    // headers: {'X-Custom-Header': 'foobar'}
-  });
-export default wikiInstance;
